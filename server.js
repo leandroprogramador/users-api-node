294bf8cc5344ee7fd49bbd8063dfa1b8872e3484
Leandro Araujo <lean.ib@hotmail.com>
2025-05-27 11:30:47 -0300

    adjusts to connect with frontend

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import express, { response } from 'express'
 import { PrismaClient } from './generated/prisma/index.js'
+import cors from 'cors'
+
 
 const prisma = new PrismaClient()
-
 const app = express()
 app.use(express.json())
+app.use(cors())
+
+
+
 
 
 app.get('/users', async (req, res) => {
